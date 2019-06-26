32ffa82da43fe393c2f2fd0eac4267d7ddc7c019
deepak <deepak>
2019-06-26 07:16:54 +0000

    added new code for c

--- build/libs/Logger/cmake_install.cmake
+++ build/libs/Logger/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/humble_d/Projects/Sandbox/sample_cmake/libs/Logger
+# Install script for directory: /home/ec2-user/c-pjt/sample_cmake/libs/Logger
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,11 +29,6 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
-# Is this installation the result of a crosscompile?
-if(NOT DEFINED CMAKE_CROSSCOMPILING)
-  set(CMAKE_CROSSCOMPILING "FALSE")
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
 endif()
 
